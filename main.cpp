cbf6fbdf91ea5672b08587ab714ca4fe584f31cf
NlodeL <nlodelop@myuax.com>
2023-11-15 20:39:53 +0100

    Agrego las nuevas funciones al main para probarlas

--- main.cpp
+++ main.cpp
@@ -5,7 +5,10 @@ using namespace std;
 using namespace Matematicas;
 
 int main()
-{
+{ // Ejercicio 1
     cout << "Suma " << suma(5,3) << endl;
     cout << "Resta " << resta(5,3) << endl;
+    cout << "Multiplicacion " << multiplicacion(5,3) << endl;
+    cout << "Division " << division(5,3) << endl;
+
 }
