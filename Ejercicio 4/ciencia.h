e334e64551a670b13b071cc8e08f69609790d049
NlodeL <nlodelop@myuax.com>
2023-11-15 23:42:00 +0100

    con ayuda de los apuntes y de github copilot cree la funcion de calculo de energía

--- Ejercicio 4/ciencia.h
+++ Ejercicio 4/ciencia.h
@@ -9,7 +9,9 @@ namespace Ciencia {
     namespace Fisica
     {
         const double L = 299792458; //km/s
-        
+        double calcularEnergia(double masa) {
+            return masa * L * L;
+        }
     }
 }
 
