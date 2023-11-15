1eb0004d1971b71c23c0da9e40c99a307ec790e8
NlodeL <nlodelop@myuax.com>
2023-11-15 23:37:03 +0100

    creo el .cpp y .h de Ciencia y en este creo su respectivo namespace en el que creo el subtipo Fisica

--- Ejercicio 4/ciencia.cpp
+++ Ejercicio 4/ciencia.cpp
@@ -1,8 +1,5 @@
 //
 // Created by nicol on 15/11/2023.
 //
-int main() {
 
-
-    return 0;
-}
+#include "ciencia.h"
---
+++ Ejercicio 4/ciencia.h
@@ -0,0 +1,15 @@
+//
+// Created by nicol on 15/11/2023.
+//
+
+#ifndef COLECCI_N_DE_EJERCICIOS_STL_CIENCIA_H
+#define COLECCI_N_DE_EJERCICIOS_STL_CIENCIA_H
+
+namespace Ciencia {
+    namespace Fisica
+    {
+    }
+}
+
+
+#endif //COLECCI_N_DE_EJERCICIOS_STL_CIENCIA_H
