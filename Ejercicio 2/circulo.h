ad5452cde570fb0961b72c403da343fc1dd0df91
NlodeL <nlodelop@myuax.com>
2023-11-15 22:45:13 +0100

    comienzo creando los .cpp y .h para circulo y triangulo

---
+++ Ejercicio 2/circulo.h
@@ -0,0 +1,14 @@
+//
+// Created by nicol on 15/11/2023.
+//
+
+#ifndef COLECCI_N_DE_EJERCICIOS_STL_CIRCULO_H
+#define COLECCI_N_DE_EJERCICIOS_STL_CIRCULO_H
+
+
+class circulo {
+
+};
+
+
+#endif //COLECCI_N_DE_EJERCICIOS_STL_CIRCULO_H
---
+++ Ejercicio 2/triangulo.h
@@ -0,0 +1,14 @@
+//
+// Created by nicol on 15/11/2023.
+//
+
+#ifndef COLECCI_N_DE_EJERCICIOS_STL_TRIANGULO_H
+#define COLECCI_N_DE_EJERCICIOS_STL_TRIANGULO_H
+
+
+class triangulo {
+
+};
+
+
+#endif //COLECCI_N_DE_EJERCICIOS_STL_TRIANGULO_H
